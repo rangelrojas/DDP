c275f42b84f141631faa5d4cfd6b5b22aa86d97d
rangelrojas <rangelrojas@unicesar.edu.co>
2023-05-04 19:22:38 -0500

    Resouesta todo correcto

--- Main.py
+++ Main.py
@@ -1,11 +1,26 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
+@app.get("/hello")
 def Hello():
     return {"HELLO FASTAPI"}
 
+@app.get("/Isprime")
+def Isprime(numero): 
+     for n in range(2,numero):
+       if numero % n == 0:
+          return False  
+     return True
 
-def Isprime(numero):  
-     return False
-
+@app.get("/Fibonnacci")
 def Fibonnacci(numero: int):
-     return 8
+     a=0
+     b=1
+     for n in range(0, numero):
+        c=a+b
+        a=b
+        b=c
+     return a
--- Pruebas.py
+++ Pruebas.py
@@ -1,12 +1,13 @@
+from Main import *
 
 def test_Hello():
     saludo = Hello()
     assert saludo == "HELLO FASTAPI"
     
 def test_Isprime():
-    numero=Isprime({10})
+    numero=Isprime(6)
     assert numero == False
     
 def test_Fibonnacci():
-    numero=Fibonnacci(8)
-    assert numero == 6
+    numero=Fibonnacci(4)
+    assert numero == 3
